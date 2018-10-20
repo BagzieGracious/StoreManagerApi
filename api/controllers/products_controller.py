fbb199a1a79362a4201be63eef2f6a30a639b446
Bagenda Deogracious <bagendadeogracious@gmail.com>
2018-10-20 10:59:06 +0300

    add product controller [#161312394, #161312666, #161312785]

---
+++ api/controllers/products_controller.py
@@ -0,0 +1,44 @@
+"""
+Module to handle products requests via endpoints
+"""
+
+from flask import request, jsonify
+from flask.views import MethodView
+from api.models.product_model import ProductModel
+from api.config.data_validation import DataValidation
+
+class ProductsController(MethodView):
+    """
+    Products controller that inherits the method view
+    """
+    allProducts = ProductModel.get_products_data()
+
+    @staticmethod
+    def get(product_id=None):
+        """
+        Method that handles view of products
+        """
+        if product_id is None:
+            return jsonify(ProductModel.get_products_data()), 200
+        return ProductModel.get_products_data(product_id)
+
+    @staticmethod
+    def post():
+        """
+        Method that handles create products
+        """
+        post_data = request.get_json()
+
+        post_values = DataValidation.validate_product(post_data['quantity'], post_data['minQuantity'], post_data['product'], post_data['description'], post_data['productDateAdded'])
+
+        if isinstance(post_values, bool):
+            data_object = {
+                "product_id": len(ProductsController.allProducts) + 1,
+                "product": post_data['product'],
+                "description": post_data['description'],
+                "quantity": post_data['quantity'],
+                "minQuantity": post_data['minQuantity'],
+                "productDateAdded": post_data['productDateAdded'],
+            }
+            return ProductModel.create_product(data_object)
+        return post_values
