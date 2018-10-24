f7a9cf124bb21f5f73ca81c647d11f1fbb9945e4
Bagenda Deogracious <bagendadeogracious@gmail.com>
2018-10-25 02:59:37 +0300

    update data file

--- api/data/data.py
+++ api/data/data.py
@@ -15,42 +15,32 @@ class Data:
             {
                 "product_id":1,
                 "product":"Iphone",
-                "description":"The iPhone is a smartphone developed by Apple foundation",
                 "quantity":100,
                 "minQuantity":3,
-                "productDateAdded":"20:30 03-Jan-2018"
             },
             {
                 "product_id":2,
                 "product":"HTC",
-                "description":"The HTC U12 Life is powered by 1.8GHz",
                 "quantity":120,
-                "minQuantity":5,
-                "productDateAdded":"20:30 03-Feb-2018"
+                "minQuantity":5
             },
             {
                 "product_id":3,
                 "product":"Samsung",
-                "description":"The Samsung Galaxy S8 is powered by 1.9GHz",
                 "quantity":324,
-                "minQuantity":2,
-                "productDateAdded":"20:30 03-Mar-2018"
+                "minQuantity":2
             },
             {
                 "product_id":4,
                 "product":"Nokia",
-                "description":"The phone comes with a 6.00-inch",
                 "quantity":165,
-                "minQuantity":10,
-                "productDateAdded":"20:30 03-Apr-2018"
+                "minQuantity":10
             },
             {
                 "product_id":5,
                 "product":"Motorola",
-                "description":"The phone comes with a 4.50-inch touchscreen",
                 "quantity":986,
-                "minQuantity":4,
-                "productDateAdded":"20:30 03-May-2018"
+                "minQuantity":4
             }
         ]
 
@@ -64,35 +54,30 @@ class Data:
                 "sale_id": 1,
                 "product": "Samsung Flatscreen Tv",
                 "quantity": 2,
-                "price": 4500000,
-                "salesAddedDate": "20:30 02-Jan-2018"
+                "price": 4500000
             },
             {
                 "sale_id": 2,
                 "product": "Toshiba Flatscreen Tv",
                 "quantity": 6,
-                "price": 9000000,
-                "salesAddedDate": "20:30 02-Feb-2018"
+                "price": 9000000
             },
             {
                 "sale_id": 3,
                 "product": "LG Flatscreen Tv",
                 "quantity": 12,
-                "price": 1500000,
-                "salesAddedDate": "20:30 02-Mar-2018"
+                "price": 1500000
             },
             {
                 "sale_id": 4,
                 "product": "Sony Flatscreen Tv",
                 "quantity": 1,
-                "price": 500000,
-                "salesAddedDate": "20:30 02-Apr-2018"
+                "price": 500000
             },
             {
                 "sale_id": 5,
                 "product": "Hisense Flatscreen Tv",
                 "quantity": 2,
-                "price": 800000,
-                "salesAddedDate": "20:30 02-May-2018"
+                "price": 800000
             },
         ]
