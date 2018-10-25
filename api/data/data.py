09423d912afb1a81bccd9b8d5e9ba1ecff1f0fe9
Bagenda Deogracious <bagendadeogracious@gmail.com>
2018-10-25 19:16:48 +0300

    update data file

--- api/data/data.py
+++ api/data/data.py
@@ -15,30 +15,35 @@ class Data:
             {
                 "product_id":1,
                 "product":"Iphone",
+                "description":"The iPhone is a smartphone developed by Apple foundation",
                 "quantity":100,
-                "minQuantity":3,
+                "minQuantity":3
             },
             {
                 "product_id":2,
                 "product":"HTC",
+                "description":"The HTC U12 Life is powered by 1.8GHz",
                 "quantity":120,
                 "minQuantity":5
             },
             {
                 "product_id":3,
                 "product":"Samsung",
+                "description":"The Samsung Galaxy S8 is powered by 1.9GHz",
                 "quantity":324,
                 "minQuantity":2
             },
             {
                 "product_id":4,
                 "product":"Nokia",
+                "description":"The phone comes with a 6.00-inch",
                 "quantity":165,
                 "minQuantity":10
             },
             {
                 "product_id":5,
                 "product":"Motorola",
+                "description":"The phone comes with a 4.50-inch touchscreen",
                 "quantity":986,
                 "minQuantity":4
             }
