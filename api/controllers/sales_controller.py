e3e7e11982595b4e16a2d0b824232a510eb3571a
Bagenda Deogracious <bagendadeogracious@gmail.com>
2018-10-25 19:15:45 +0300

    add updated sales controller file

---
+++ api/controllers/sales_controller.py
@@ -0,0 +1,41 @@
+"""
+Module to handle sales requests via endpoints
+"""
+
+from flask import request, jsonify
+from flask.views import MethodView
+from api.models.sales_model import SalesModel
+from api.config.data_validation import DataValidation
+
+class SalesController(MethodView):
+    """
+    Sales controller that inherits the method view
+    """
+    all_sales = SalesModel.get_all_sales()
+
+    @staticmethod
+    def get(sale_id=None):
+        """
+        Method that handles view of sales records
+        """
+        if sale_id is None:
+            return jsonify({"success":True, "payload":SalesModel.get_all_sales()}), 200
+        return SalesModel.get_all_sales(sale_id)
+
+    @staticmethod
+    def post():
+        """
+        Method that handles create sales
+        """
+        post_data = request.get_json()
+        post_values = DataValidation.validate_sale(post_data['quantity'], post_data['price'], post_data['product'])
+
+        if isinstance(post_values, bool):
+            data_object = {
+                "sale_id": len(SalesController.all_sales) + 1,
+                "product": post_data['product'],
+                "quantity": post_data['quantity'],
+                "price": post_data['price']
+            }
+            return SalesModel.create_sales(data_object)
+        return post_values
